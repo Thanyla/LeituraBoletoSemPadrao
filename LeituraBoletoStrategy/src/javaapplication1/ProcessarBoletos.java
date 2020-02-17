b62f6dcf32c69c3bb011f8f5c7484fdfac6b68ab
Thányla Sales <thanylas@gmail.com>
2020-02-17 10:22:46 -0300

    Refatoração de variaveis

--- LeituraBoletoStrategy/src/javaapplication1/ProcessarBoletos.java
+++ LeituraBoletoStrategy/src/javaapplication1/ProcessarBoletos.java
@@ -8,11 +8,13 @@ import java.util.List;
  * @author narizinho
  */
 public abstract class ProcessarBoletos {
+
     protected abstract List<Boleto> lerArquivo(String nomeArquivo);
-     DateTimeFormatter FORMATO_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    DateTimeFormatter FORMATO_DATA_HORA = 
-            DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
-    public void processar(String nomeArquivo){
+    public static final DateTimeFormatter FORMATO_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final DateTimeFormatter FORMATO_DATA_HORA
+            = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+
+    public void processar(String nomeArquivo) {
         List<Boleto> boletos = lerArquivo(nomeArquivo);
         boletos.forEach((boleto) -> {
             System.out.println("Id: " + boleto.getId()
